64b1bfd412c94d60b9b594f6efc7f07103c94dab
mikksillaste <mikk.sillaste@gmail.com>
2018-08-17 17:40:25 +0300

    video 96(11:33)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
 class App extends Component {
     constructor() {
@@ -11,21 +14,17 @@ class App extends Component {
     };
 
     onRouteChange = (route) => {
-
+        this.setState({ route: route});
     };
 
     render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Page1 onRouteChange={this.onRouteChange()}/>
-        <Page2 />
-        <Page3 />
-      </div>
-    );
+        if (this.state.route === 'page1') {
+            return <Page1 onRouteChange={this.onRouteChange}/>
+        } else if (this.state.route === 'page2') {
+            return <Page2 onRouteChange={this.onRouteChange}/>
+        } else if (this.state.route === 'page3') {
+            return <Page3 onRouteChange={this.onRouteChange}/>
+        }
   }
 }
 
