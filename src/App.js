e194d937bf520a3a7bc3b3ef7025359468f52ff3
mikksillaste <mikk.sillaste@gmail.com>
2018-08-17 14:01:53 +0300

    Video 96(6:03)

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            route: 'page1'
+        }
+    };
+
+    onRouteChange = (route) => {
+
+    };
+
+    render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Page1 onRouteChange={this.onRouteChange()}/>
+        <Page2 />
+        <Page3 />
+      </div>
+    );
+  }
+}
+
+export default App;
